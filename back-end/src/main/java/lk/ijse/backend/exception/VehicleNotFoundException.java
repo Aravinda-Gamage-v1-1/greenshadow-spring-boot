39234b59be86ed7d52d8e87f845d7d9f1c3a3991
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-11-27 22:31:18 +0530

    add user and vehicle exceptions

---
+++ back-end/src/main/java/lk/ijse/backend/exception/VehicleNotFoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.backend.exception;
+
+public class VehicleNotFoundException extends RuntimeException{
+    public VehicleNotFoundException() {
+        super();
+    }
+
+    public VehicleNotFoundException(String message) {
+        super(message);
+    }
+
+    public VehicleNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
