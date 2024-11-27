f2af40c21e87bebf6df9be29e3c3b32b84f46e7c
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-11-27 22:47:45 +0530

    add BaseService

--- back-end/src/main/java/lk/ijse/backend/BackEndApplication.java
+++ back-end/src/main/java/lk/ijse/backend/BackEndApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackEndApplication {
@@ -9,5 +11,6 @@ public class BackEndApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackEndApplication.class, args);
     }
-
+    @Bean
+    public ModelMapper modelMapper() {return new ModelMapper();}
 }
