094ebbc3f97878478dd227873a76aa2871390983
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-11-28 21:30:39 +0530

    add HealthTestController

--- back-end/src/main/java/lk/ijse/backend/controller/HealthTestController.java
+++ back-end/src/main/java/lk/ijse/backend/controller/HealthTestController.java
@@ -1,11 +1,13 @@
 package lk.ijse.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/health")
+@CrossOrigin
 public class HealthTestController {
     @GetMapping
     public String health() {return "HealthTest controller is running";}
