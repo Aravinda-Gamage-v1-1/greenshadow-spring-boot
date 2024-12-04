91b9d4681c63aba2ca6be24e9827d208b6e91e4d
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-12-04 14:38:13 +0530

    update CropServiceImpl

--- back-end/src/main/java/lk/ijse/backend/service/impl/CropServiceImpl.java
+++ back-end/src/main/java/lk/ijse/backend/service/impl/CropServiceImpl.java
@@ -3,6 +3,7 @@ package lk.ijse.backend.service.impl;
 import lk.ijse.backend.dto.impl.CropDTO;
 import lk.ijse.backend.entity.CropEntity;
 import lk.ijse.backend.entity.FieldEntity;
+import lk.ijse.backend.exception.CropNotFoundException;
 import lk.ijse.backend.repository.CropRepo;
 import lk.ijse.backend.repository.FieldRepo;
 import lk.ijse.backend.repository.UserRepo;
@@ -10,6 +11,7 @@ import lk.ijse.backend.service.CropService;
 import lk.ijse.backend.util.AppUtil;
 import lk.ijse.backend.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,12 +29,14 @@ public class CropServiceImpl implements CropService {
     private Mapping mapping;
 
     @Override
+    @PreAuthorize("hasRole('MANAGER') or hasRole('SCIENTIST')")
     public CropDTO save(CropDTO dto) {
         dto.setId(AppUtil.generateCropId());
         return mapping.toCropDTO(cropRepo.save(mapping.toCropEntity(dto)));
     }
 
     @Override
+    @PreAuthorize("hasRole('MANAGER') or hasRole('SCIENTIST')")
     public CropDTO update(String id, CropDTO dto) {
         CropEntity existingCrop = cropRepo.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Crop not found with ID: " + id));
@@ -60,11 +64,19 @@ public class CropServiceImpl implements CropService {
     }
 
     @Override
+    @PreAuthorize("hasRole('MANAGER') or hasRole('SCIENTIST')")
     public void delete(String id) {
+        CropEntity crop = cropRepo.findById(id)
+                .orElseThrow(() -> new CropNotFoundException("Crop not found with ID: " + id));
+
+        // Remove associations with staff members
+        crop.getLogs().forEach(log -> log.getCropLogs().remove(crop));
+        crop.getLogs().clear();
         cropRepo.deleteById(id);
     }
 
     @Override
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATOR') or hasRole('SCIENTIST')")
     public CropDTO findById(String id) {
         Optional<CropEntity> byId = cropRepo.findById(id);
         if (byId.isPresent()){
@@ -74,6 +86,7 @@ public class CropServiceImpl implements CropService {
     }
 
     @Override
+    @PreAuthorize("hasRole('MANAGER') or hasRole('ADMINISTRATOR') or hasRole('SCIENTIST')")
     public List<CropDTO> findAll() {
         return mapping.asCropDTOList(cropRepo.findAll());
     }
